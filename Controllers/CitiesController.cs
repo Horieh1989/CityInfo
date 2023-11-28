2ef2703526720b1ed2c25a374f97e99c29f91f33
Horieh Ghiasidalalian <horiehghiasi@gmail.com>
2023-11-28 10:27:28 +0100

    Add project files.

---
+++ Controllers/CitiesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography.X509Certificates;
+
+namespace CityInfo.Controllers
+{
+    [ApiController]//very important
+    public class CitiesController:ControllerBase
+    {
+        [HttpGet("api/Cities")]
+        public JsonResult GetCities()
+        {
+            return new JsonResult(
+                new List<object> { 
+            
+                new{id=1,name="New York City"},
+                new { id = 2, name = "Antwerp" }
+            });
+
+        }
+           
+        
+    }
+}
