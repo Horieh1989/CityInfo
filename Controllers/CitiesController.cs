598c03f6e6ebfbdc54094e80e5625f078c393a85
Horieh Ghiasidalalian <horiehghiasi@gmail.com>
2023-11-29 08:44:21 +0100

    make a city controller, and citiesDatabase,

--- Controllers/CitiesController.cs
+++ Controllers/CitiesController.cs
@@ -1,23 +1,35 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CityInfo.Models;
+using Microsoft.AspNetCore.Mvc;
 using System.Security.Cryptography.X509Certificates;
 
 namespace CityInfo.Controllers
 {
-    [ApiController]//very important
+    [ApiController] //very important
+    [Route("api/cities")]//for the whole controller
     public class CitiesController:ControllerBase
     {
-        [HttpGet("api/Cities")]
-        public JsonResult GetCities()
+        [HttpGet]
+        public ActionResult<IEnumerable<CityDto>> GetCities()
         {
-            return new JsonResult(
-                new List<object> { 
-            
-                new{id=1,name="New York City"},
-                new { id = 2, name = "Antwerp" }
-            });
-
+            return Ok(CitiesDataStore.current.Cities);
         }
-           
-        
+
+        [HttpGet("{id}")]
+        //public JsonResult GetCity(int id) 
+        //{ 
+        //  return new JsonResult(CitiesDataStore.current.Cities.FirstOrDefault(c => c.Id == id));
+        //}
+        // we can write instead this:
+        public ActionResult<CityDto> GetCity (int id)
+        {
+            //find city
+            var cityToReturn=CitiesDataStore.current.Cities.FirstOrDefault (c => c.Id == id);
+            if ( cityToReturn==null)
+
+            {
+                return NotFound();
+            }
+            return Ok(cityToReturn);
+        }
     }
 }
---
+++ Controllers/PointOfInterestController.cs
@@ -0,0 +1,44 @@
+﻿using CityInfo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.Controllers
+{
+    [Route("api/cities/{cityId}/pointsofinterest")]// for the whole controller
+    [ApiController]
+    public class PointsOfInterestController : ControllerBase
+    {
+       
+        public ActionResult<IEnumerable<PointOfInterestDto>> GetPointsOfIntrest(int cityid)
+        { 
+            var city= CitiesDataStore.current.Cities.FirstOrDefault(c=> c.Id==cityid);
+            if (city == null)
+            {
+                return NotFound();
+
+            }
+            return Ok(city.PointsOfInterest);
+        }
+        [HttpGet("{pointOfIntrestId}")]
+        public ActionResult<IEnumerable<PointOfInterestDto>> GetPoinOfIntrest(int cityId, int pointOfIntrestId) 
+        {
+            var city = CitiesDataStore.current.Cities.FirstOrDefault(c => c.Id == cityId);
+            if (city == null)
+            {
+                return NotFound();
+
+            }
+            // Find pointOfIntrest
+            var pointOfIntrest = CitiesDataStore.current.Cities.FirstOrDefault(c => c.Id == pointOfIntrestId);
+            if (pointOfIntrest == null)
+            {
+                return NotFound();
+
+            }
+            return Ok(pointOfIntrest);
+
+
+        }
+
+    }
+}
